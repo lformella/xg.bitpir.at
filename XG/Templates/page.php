9acbb4913cf51383220e7ae3f0b0d978b570c773
Lars Formella <ich@larsformella.de>
2012-05-25 21:17:49 +0200

    forkme tag

--- XG/Templates/page.php
+++ XG/Templates/page.php
@@ -28,6 +28,10 @@
 
 	<body>
 
+		<a href="https://github.com/lformella/xg.bitpir.at">
+			<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub">
+		</a>
+
 		<div id="author-tag"></div>
 
 		<div class="menu-box">
