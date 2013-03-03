1d93a5c6185c6d8404c7dd0b49c44ea08d0ec4e7
Lars Formella <ich@larsformella.de>
2013-03-03 10:07:58 +0100

    change piwik tracking url

--- XG/Templates/page.php
+++ XG/Templates/page.php
@@ -118,7 +118,7 @@
 
 		<!-- Piwik -->
 		<script type="text/javascript">
-			var pkBaseURL = (("https:" == document.location.protocol) ? "https://52g.de/piwik/" : "http://52g.de/piwik/");
+			var pkBaseURL = (("https:" == document.location.protocol) ? "http://piwik.bitpir.at/" : "http://piwik.bitpir.at/");
 			document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
 		</script>
 		<script type="text/javascript">
